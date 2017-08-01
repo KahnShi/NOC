9d3b86343331f93bf401e4ee9f2e8dac182edec0
KahnShi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2017-08-01 20:37:43 +0900

    Data structure and backward upate of Finite Discrete LQR.

---
+++ lqr_control/include/lqr_control/LqrFD.h
@@ -0,0 +1,85 @@
+// -*- mode: c++ -*-
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, JSK Lab
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/o2r other materials provided
+ *     with the distribution.
+ *   * Neither the name of the JSK Lab nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#ifndef LQR_FINITE_DISCRETE_controller_H
+#define LQR_FINITE_DISCRETE_controller_H
+
+/* ros */
+#include <ros/ros.h>
+
+/* linear algebra */
+#include <math.h>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/LU>
+#include <eigen3/Eigen/Geometry>
+#include <eigen3/Eigen/Eigenvalues>
+
+
+/* general header file */
+#include <iostream>
+
+using namespace Eigen;
+namespace lqr_finite_discrete
+{
+  class LqrFiniteDiscreteControl{
+  public:
+    LqrFiniteDiscreteControl(ros::NodeHandle nh, ros::NodeHandle nhp);
+    ~LqrFiniteDiscreteControl();
+
+  private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle nhp_;
+
+    ros::Timer  control_timer_;
+
+    double control_freq_;
+    double end_time_;
+    int iteration_times_;
+    int u_size_;
+    int s_size_;
+    MatrixXd *A_ptr_;
+    MatrixXd *B_ptr_;
+    VectorXd *input_ptr_;
+    VectorXd *control_ptr_;
+    MatrixXd *Q_ptr_;
+    MatrixXd *R_ptr_;
+    std::vector<MatrixXd *> F_ptr_vec_;
+
+    void initLQR(double freq, double period, MatrixXd *A, MatrixXd *B, MatrixXd *Q, MatrixXd *R);
+    void backwardIteration();
+  };
+}
+#endif
---
+++ lqr_control/src/LqrFD.cpp
@@ -0,0 +1,87 @@
+// -*- mode: c++ -*-
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, JSK Lab
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/o2r other materials provided
+ *     with the distribution.
+ *   * Neither the name of the JSK Lab nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#include <lqr_control/LqrFD.h>
+
+namespace lqr_finite_discrete
+{
+  LqrFiniteDiscreteControl::LqrFiniteDiscreteControl(ros::NodeHandle nh, ros::NodeHandle nhp): nh_(nh), nhp_(nhp){
+  }
+
+  void LqrFiniteDiscreteControl::initLQR(double freq, double period, MatrixXd *A, MatrixXd *B, MatrixXd *Q, MatrixXd *R){
+    control_freq_ = freq;
+    if (floor(freq * period) < freq * period){
+      end_time_ = (floor(freq * period) + 1.0) / freq;
+      iteration_times_ = floor(freq * period) + 1;
+    }
+    else{
+      end_time_ = period;
+      iteration_times_ = floor(freq * period);
+    }
+    s_size_ = Q_ptr_->rows();
+    u_size_ = R_ptr_->rows();
+    A_ptr_ = new MatrixXd(s_size_, s_size_);
+    B_ptr_ = new MatrixXd(u_size_, u_size_);
+    Q_ptr_ = new MatrixXd(s_size_, s_size_);
+    R_ptr_ = new MatrixXd(u_size_, u_size_);
+    for (int i = 0; i < s_size_; ++i)
+      for (int j = 0; j < s_size_; ++j){
+        (*A_ptr_)(i, j) = (*A)(i, j);
+        (*Q_ptr_)(i, j) = (*Q)(i, j);
+      }
+    for (int i = 0; i < u_size_; ++i)
+      for (int j = 0; j < u_size_; ++j){
+        (*B_ptr_)(i, j) = (*B)(i, j);
+        (*R_ptr_)(i, j) = (*R)(i, j);
+      }
+  }
+
+  void LqrFiniteDiscreteControl::backwardIteration(){
+    VectorXd P = MatrixXd::Zero(s_size_, s_size_);
+    // todo: assume N is zero, namely do not have x^T *N*u in cost function
+    VectorXd N = MatrixXd::Zero(s_size_, u_size_);
+    P = *Q_ptr_;
+    for (int i = 0; i < iteration_times_; ++i){
+      MatrixXd *F_ptr = new MatrixXd(u_size_, s_size_);
+      MatrixXd Fk_1 = ((*R_ptr_) + B_ptr_->transpose() * P * (*B_ptr_)).inverse();
+      MatrixXd Fk_2 = B_ptr_->transpose() * P * (*A_ptr_) + N.transpose();
+      (*F_ptr) = Fk_1 * Fk_2;
+      P = A_ptr_->transpose() * P * (*A_ptr_)
+        - (A_ptr_->transpose() * P * (*B_ptr_) + N.transpose()) * (*F_ptr)
+        + (*Q_ptr_);
+      F_ptr_vec_.push_back(F_ptr);
+    }
+  }
+}
