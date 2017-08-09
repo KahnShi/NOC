504c73f5453fbf219236cc2484ed95a3a1f5e659
KahnShi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2017-08-09 14:46:48 +0900

    Add updateNewState function.

--- lqr_control/src/LqrFD_Quadrotor.cpp
+++ lqr_control/src/LqrFD_Quadrotor.cpp
@@ -102,7 +102,7 @@ namespace lqr_discrete{
     for (int i = 0; i < 4; ++i)
       (*u0_ptr_)(i) = (*un_ptr_)(i) = uav_mass_ * 9.78 / 4.0;
 
-    debug_ = false;
+    debug_ = true;
   }
 
   void LqrFiniteDiscreteControlQuadrotor::updateAll(){
@@ -167,11 +167,18 @@ namespace lqr_discrete{
         }
       }
 
+      u_vec_.push_back(u);
+      // method 1: x(k+1) = A*x(k) + B*u(k)
       VectorXd x = (*A_ptr_) * (*x_ptr_)
         + (*B_ptr_) * u;
       x_vec_.push_back(Vector3d(x(0), x(1), x(2)));
       *x_ptr_ = x;
-      u_vec_.push_back(u);
+
+      // method 2: x(k+1) = f(x(k), u(k))
+      // VectorXd *x = new VectorXd(x_size_);
+      // updateNewState(x);
+      // x_vec_.push_back(Vector3d((*x)(0), (*x)(1), (*x)(2)));
+      // *x_ptr_ = *x;
 
     }
   }
@@ -315,7 +322,65 @@ namespace lqr_discrete{
 
     (*B_ptr_) = (*B_ptr_) / control_freq_;
   }
+  void LqrFiniteDiscreteControlQuadrotor::updateNewState(VectorXd *new_x_ptr){
+    VectorXd dev_x(x_size_);
+    for (int i = 0; i < x_size_; ++i)
+      dev_x(i) = 0.0;
+    /* x, y, z */
+    dev_x(P_X) = (*x_ptr_)(V_X);
+    dev_x(P_Y) = (*x_ptr_)(V_Y);
+    dev_x(P_Z) = (*x_ptr_)(V_Z);
 
+    /* v_x, v_y, v_z */
+    double u = 0.0;
+    for (int i = 0; i < u_size_; ++i)
+      u += (*u_ptr_)[i];
+    /* u' = u / m */
+    u = u / uav_mass_;
+    /* d v_x = (2 * q_w * q_y + 2 * q_x * q_z) * u' */
+    dev_x(V_X) = (2 * (*x_ptr_)[Q_W] * (*x_ptr_)[Q_Y]
+                  + 2 * (*x_ptr_)[Q_X] * (*x_ptr_)[Q_Z]) * u;
+    /* d v_y = (2 * q_w * q_x + 2 * q_y * q_z) * u' */
+    dev_x(V_Y) = (2 * (*x_ptr_)[Q_W] * (*x_ptr_)[Q_X]
+                  + 2 * (*x_ptr_)[Q_Y] * (*x_ptr_)[Q_Z]) * u;
+    /* d v_z = (1 - 2 * q_x * q_x - 2 * q_y * q_y) * u' */
+    dev_x(V_Z) = (1 - 2 * (*x_ptr_)[Q_X] * (*x_ptr_)[Q_X]
+                  - 2 * (*x_ptr_)[Q_Y] * (*x_ptr_)[Q_Y]) * u - 9.78;
+
+    /* q_w, q_x, q_y, q_z */
+    /* d q = 1/2 * q * [0, w]^T (the multiply opeation is under quaternion multiply) */
+    /* d q_w = 1/2 (q_w * 0 - q_x * w_x - q_y * w_y - q_z * w_z) */
+    dev_x(Q_W) = (-(*x_ptr_)[Q_X] * (*x_ptr_)[W_X]
+                  -(*x_ptr_)[Q_Y] * (*x_ptr_)[W_Y]
+                  -(*x_ptr_)[Q_Z] * (*x_ptr_)[W_Z])/ 2.0;
+    /* d q_x = 1/2 (q_w * w_x + q_x * 0 + q_y * w_z - q_z * w_y) */
+    dev_x(Q_X) = ((*x_ptr_)[Q_W] * (*x_ptr_)[W_X]
+                  +(*x_ptr_)[Q_Y] * (*x_ptr_)[W_Z]
+                  -(*x_ptr_)[Q_Z] * (*x_ptr_)[W_Y])/ 2.0;
+    /* d q_y = 1/2 (q_w * w_y - q_x * w_z + q_y * 0 + q_z * w_x) */
+    dev_x(Q_Y) = ((*x_ptr_)[Q_W] * (*x_ptr_)[W_Y]
+                  -(*x_ptr_)[Q_X] * (*x_ptr_)[W_Z]
+                  +(*x_ptr_)[Q_Z] * (*x_ptr_)[W_X])/ 2.0;
+    /* d q_z = 1/2 (q_w * w_z + q_x * w_y - q_y * w_x + q_z * 0) */
+    dev_x(Q_Z) = ((*x_ptr_)[Q_W] * (*x_ptr_)[W_Z]
+                  +(*x_ptr_)[Q_X] * (*x_ptr_)[W_Y]
+                  -(*x_ptr_)[Q_Y] * (*x_ptr_)[W_X])/ 2.0;
+
+    /* w_x, w_y, w_z */
+    /* d w = I^-1 * (- (w^) * (Iw) + M_para * [u1;u2;u3;u4]), w^ = [0, -w_z, w_y; w_z, 0, -w_x; -w_y, w_x, 0] */
+    /* d w_w = I^-1 * (- d(w^) * (Iw) - (w^) * (I * d(w))) */
+    Vector3d w((*x_ptr_)[W_X], (*x_ptr_)[W_Y], (*x_ptr_)[W_Z]);
+    MatrixXd w_m = MatrixXd::Zero(3, 3);
+    w_m(0, 1) = -(*x_ptr_)[W_Z]; w_m(0, 2) = (*x_ptr_)[W_Y];
+    w_m(1, 0) = (*x_ptr_)[W_Z]; w_m(1, 2) = -(*x_ptr_)[W_X];
+    w_m(2, 0) = -(*x_ptr_)[W_Y]; w_m(2, 1) = (*x_ptr_)[W_X];
+    Vector3d dw;
+    dw = I_ptr_->inverse() * (-w_m * (*I_ptr_) * w + (*M_para_ptr_) * (*u_ptr_));
+    for (int i = 0; i < 3; ++i)
+      dev_x(W_X + i) = dw(i);
+
+    *new_x_ptr = dev_x / control_freq_ + *x_ptr_;
+  }
 }
 
 
