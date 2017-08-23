Make higher weight on z and correct Q matrix calculation.

lqr_control/script/dareSolver.py
@@ -55,13 +55,12 @@ class dareSolver:
         rospy.loginfo("[Dare Solver]Receive and start to solve Riccati equation.")
         x_size = req.A.array.layout.dim[0].size
         u_size = req.B.array.layout.dim[1].size
-        R = 50.0 * np.matlib.eye(u_size, dtype=float)
+        R = 400.0 * np.matlib.eye(u_size, dtype=float)
         Q = np.matlib.eye(x_size, dtype=float)
         for i in range(0, 6):
             Q[i, i] = 10.0
         ## test: weight on z
-        # Q[2, 2] = 20.0
-        # Q[5, 5] = 20.0
+        Q[2, 2] = Q[5, 5] = 100.0
         A = np.matlib.zeros((x_size, x_size))
         B = np.matlib.zeros((x_size, u_size))
 