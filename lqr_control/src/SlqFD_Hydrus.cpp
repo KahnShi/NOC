34928455ff60e338ee4f6fa7c750f469913a0728
KahnShi <shifan@jsk.imi.i.u-tokyo.ac.jp>
2017-11-06 23:14:44 +0900

    Fix bug in cur_time and support waypoint value.

--- lqr_control/src/SlqFD_Hydrus.cpp
+++ lqr_control/src/SlqFD_Hydrus.cpp
@@ -229,9 +229,9 @@ namespace lqr_discrete{
       u_vec_.push_back(u_init);
       double cur_time;
       if (i <= high_freq_iteration_times_)
-        cur_time = i / control_high_freq_;
+        cur_time = double(i) / control_high_freq_;
       else
-        cur_time = high_freq_iteration_times_ +
+        cur_time = high_freq_end_time_ +
           (i - high_freq_iteration_times_) / control_low_freq_;
       VectorXd cur_joint = getCurrentJoint(cur_time);
       VectorXd cur_joint_dt = getCurrentJoint(cur_time, 1);
@@ -375,7 +375,13 @@ namespace lqr_discrete{
       std::vector<MatrixXd> W_vec;
       for (int j = 1; j < waypoints_ptr_->size(); ++j){
         MatrixXd W = MatrixXd::Zero(x_size_, x_size_);
-        updateWaypointWeightMatrix(i * end_time_ / iteration_times_, (*time_ptr_)[j] - (*time_ptr_)[0], &W, j == (waypoints_ptr_->size() - 1));
+        double cur_time;
+        if (i <= high_freq_iteration_times_)
+          cur_time = double(i) / control_high_freq_;
+        else
+          cur_time = high_freq_end_time_ +
+            (i - high_freq_iteration_times_) / control_low_freq_;
+        updateWaypointWeightMatrix(cur_time, (*time_ptr_)[j] - (*time_ptr_)[0], &W, j == (waypoints_ptr_->size() - 1));
         W_vec.push_back(W);
       }
 
@@ -430,7 +436,7 @@ namespace lqr_discrete{
           std::vector<MatrixXd> W_vec;
           for (int j = 1; j < waypoints_ptr_->size(); ++j){
             MatrixXd W = MatrixXd::Zero(x_size_, x_size_);
-            updateWaypointWeightMatrix(i * end_time_ / iteration_times_, (*time_ptr_)[j] - (*time_ptr_)[0], &W, j == (waypoints_ptr_->size() - 1));
+            // updateWaypointWeightMatrix(i * end_time_ / iteration_times_, (*time_ptr_)[j] - (*time_ptr_)[0], &W, j == (waypoints_ptr_->size() - 1));
             W_vec.push_back(W);
           }
 
@@ -1193,7 +1199,13 @@ namespace lqr_discrete{
       VectorXd real_x = getAbsoluteState(&(x_vec_[i]));
       for (int j = 1; j < waypoints_ptr_->size(); ++j){
         MatrixXd W = MatrixXd::Zero(x_size_, x_size_);
-        updateWaypointWeightMatrix(i * end_time_ / iteration_times_, (*time_ptr_)[j] - (*time_ptr_)[0], &W, j == (waypoints_ptr_->size() - 1));
+        double cur_time;
+        if (i <= high_freq_iteration_times_)
+          cur_time = double(i) / control_high_freq_;
+        else
+          cur_time = high_freq_end_time_ +
+            (i - high_freq_iteration_times_) / control_low_freq_;
+        updateWaypointWeightMatrix(cur_time, (*time_ptr_)[j] - (*time_ptr_)[0], &W, j == (waypoints_ptr_->size() - 1));
         VectorXd dx_pt = stateSubtraction(&real_x, &((*waypoints_ptr_)[j]));
         cost += (dx_pt.transpose() * W * dx_pt)(0);
       }
